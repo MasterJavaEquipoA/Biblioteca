a0b731070d9c2a33e89eef402de9421f09a30ec0
DiegoMirVN <diego.miras@viewnext.com>
2024-04-22 12:52:50 +0200

    modificado usuario

--- src/com/biblioteca/clases/Usuario.java
+++ src/com/biblioteca/clases/Usuario.java
@@ -1,27 +1,30 @@
 package com.biblioteca.clases;
 
 public class Usuario {
-public String DNI;
-public String nombre;
-public boolean es_socio;
+	private String dni;
+	private String nombre;
+	private boolean esSocio;
 
-public Usuario(String DNI, String nombre,boolean es_socio) {
-	this.DNI = DNI;
-	this.nombre = nombre;
-	this.es_socio=es_socio;
-}
-public String getDNI() {
-	return DNI;
-}
-public void setDNI(String dNI) {
-	DNI = dNI;
-}
-public String getNombre() {
-	return nombre;
-}
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
+	public Usuario(String dni, String nombre, boolean esSocio) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.esSocio = esSocio;
+	}
 
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 }
