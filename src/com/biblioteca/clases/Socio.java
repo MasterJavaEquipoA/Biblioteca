4e3abac7c84573320bd0056f5795fa189117a934
DiegoMirVN <diego.miras@viewnext.com>
2024-04-22 13:00:22 +0200

    Creacion clase Socio

---
+++ src/com/biblioteca/clases/Socio.java
@@ -0,0 +1,18 @@
+package com.biblioteca.clases;
+
+public class Socio extends Usuario {
+
+	private final static int MAX_PRESTAMOS = 20;
+
+	public Socio(String dni, String nombre, boolean esSocio) {
+		super(dni, nombre, esSocio);
+
+	}
+
+	@Override
+	public String toString() {
+		return "Socio [getDni()=" + getDni() + ", getNombre()=" + getNombre() + ", getClass()="
+				+ getClass().getSimpleName() + ", toString()=" + super.toString() + "]";
+	}
+
+}
