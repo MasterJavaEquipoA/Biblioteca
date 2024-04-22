3092214644192c6c4d0351229439fbd675994143
Alvaro Babarro <alvaro.babarro@gmail.com>
2024-04-22 11:48:39 +0200

    Creada clase documento

---
+++ src/com/biblioteca/clases/Documento.java
@@ -0,0 +1,25 @@
+package com.biblioteca.clases;
+
+public class Documento {
+
+	private String codigoAlfaNum;
+	private String titulo;
+	public Documento(String codigoAlfaNum, String titulo) {
+		super();
+		this.codigoAlfaNum = codigoAlfaNum;
+		this.titulo = titulo;
+	}
+	public String getCodigoAlfaNum() {
+		return codigoAlfaNum;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	@Override
+	public String toString() {
+		return "codigoAlfaNum=" + codigoAlfaNum + ", titulo=" + titulo;
+	}
+	
+	
+	
+}
