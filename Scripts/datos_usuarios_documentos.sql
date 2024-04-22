cdde33e8631c9b19fbd5b2d0f2ff682821efc22d
DiegoMirVn <166027773+DiegoMirVn@users.noreply.github.com>
2024-04-22 10:58:38 +0200

    Add files via upload

---
+++ Scripts/datos_usuarios_documentos.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO `biblioteca`.`usuarios` (`dni`, `nombre`, `socio`) VALUES ('12345678Q', 'Paco', '0');
+INSERT INTO `biblioteca`.`usuarios` (`dni`, `nombre`, `socio`) VALUES ('22112233S', 'Hector', '1');
+INSERT INTO `biblioteca`.`usuarios` (`dni`, `nombre`, `socio`) VALUES ('12332145F', 'Alvaro', '0');
+INSERT INTO `biblioteca`.`usuarios` (`dni`, `nombre`, `socio`) VALUES ('44662211X', 'Diego', '1');
+INSERT INTO `biblioteca`.`usuarios` (`dni`, `nombre`, `socio`) VALUES ('56734598D', 'Camilo', '0');
+INSERT INTO `biblioteca`.`usuarios` (`dni`, `nombre`, `socio`) VALUES ('33221155J', 'Aaron', '1');
+INSERT INTO `biblioteca`.`usuarios` (`dni`, `nombre`, `socio`) VALUES ('99987621V', 'Maria', '0');
+
+
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('382745894-3', 'Goats', 1988, true);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('378465032-5', 'Knuckleball!', 2011, false);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('033307880-2', 'Bloody Murder', 2005, false);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('880833045-1', 'Stonewall', 1997, false);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('759275255-7', 'City Slacker', 1958, false);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('765206967-X', 'This Christmas', 2009, false);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('131859855-9', 'Quiet Man, The', 1990, false);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('615772090-5', 'Freaky Friday', 1987, true);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('303257590-7', 'It Runs in the Family (My Summer Story)', 2001, false);
+insert into biblioteca.documentos(codigoAlfaNum, titulo, anoPubli, prestado) values ('922737735-2', 'Mandabi (The Money Order)', 1996, false);
