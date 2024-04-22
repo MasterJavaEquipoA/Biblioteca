1ff804ce4cb22a9611b57fe46fc1dad95a94c0dd
Nemesiah2002 <dawdu207@gmail.com>
2024-04-22 16:14:12 +0200

    crear clase libro

--- src/com/biblioteca/clases/Libro.java
+++ src/com/biblioteca/clases/Libro.java
@@ -1,5 +1,12 @@
 package com.biblioteca.clases;
 
-public class Libro {
+public class Libro extends Documento {
+private int anhoPublicacion;
+private static final int MAX_DURACION_OCA=15;
+private static final int MAX_DURACION_SOCIO=30;
+public Libro(String codigoAlfaNum, String titulo, int anhoPublicacion) {
+	super(codigoAlfaNum, titulo);
+	this.anhoPublicacion = anhoPublicacion;
+}
 
 }
